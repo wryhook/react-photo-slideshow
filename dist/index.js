8538fcfcbe7683784f980b3c18a6afe8a6d60fff
Abdullah Raja <ahmedabdullah60@gmail.com>
2022-05-12 20:37:56 -0700

    removed bs

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Slideshow", {
+  enumerable: true,
+  get: function get() {
+    return _Slideshow.default;
+  }
+});
+
+var _Slideshow = _interopRequireDefault(require("./components/Slideshow"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
